b3cc99cc642be0c288ef677bf72b863c2d497a92
aguirrejuanse <you@example.com>
2021-07-19 20:54:28 -0300

    primer ejercicio

--- Ejercicios/02-SumArray/SumArray.js
+++ Ejercicios/02-SumArray/SumArray.js
@@ -1,6 +1,15 @@
-function SumArray(arr, n) {
-  // Your code here:
-
-}
-
-module.exports = SumArray;
+const findNeedle = (haystack, needle) => {
+  // iteramos sobre el haystack
+  for (let haystackIndex = 0; haystackIndex < haystack.length; haystackIndex += 1) {
+      // comenzamos a iterar sobre el needle
+      for (let needleIndex = 0; needleIndex < needle.length; needleIndex += 1 ) {
+          // comparamos la letra del needle en la que estamos con la letra del haystack
+          // cuando no hay match cortamos de comparar el needle
+          if (haystack[haystackIndex + needleIndex] !== needle[needleIndex]) break;
+          // si terminamos de recorrer la needle devolvemos el haystackIndex
+          if (needleIndex + 1 === needle.length) return haystackIndex;
+      }
+  }
+               // una vez que termina el loop y no encontramos match devolvemos -1
+  return -1;
+};
